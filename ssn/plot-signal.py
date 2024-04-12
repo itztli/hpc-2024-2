3de6dc0a2e197b4a99fcdd21e73064a08bd0a4fb
Victor De la Luz <vdelaluz@enesmorelia.unam.mx>
2024-04-12 10:37:16 -0600

    fft segmentation

---
+++ ssn/plot-signal.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+
+with open("ssn.dat") as f:
+    data = f.readlines()
+
+signal = []
+for line in data:
+    signal.append(float(line))
+
+plt.plot(signal)
+plt.plot(signal,'or')
+
+
+plt.show()
